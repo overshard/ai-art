65ef6b4dab5eae08a8139ccb92d1f691e71f05c3
Isaac Bythewood <isaac@bythewood.me>
2022-06-29 19:18:41 +0000

    Fix build dataset download

--- bin/sh/build.sh
+++ bin/sh/build.sh
@@ -2,7 +2,7 @@
 
 # check if ../../models/vqgan_imagenet_f16_16384.ckpt exists, if not curl it
 if [ ! -f ../../models/vqgan_imagenet_f16_16384.ckpt ]; then
-    curl https://heibox.uni-heidelberg.de/d/a7530b09fed84f80a887/files/?p=%2Fckpts%2Flast.ckpt&dl=1 -o ../../models/vqgan_imagenet_f16_16384.ckpt
+    curl -L "https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1" -o ../../models/vqgan_imagenet_f16_16384.ckpt
 fi
 
 docker-compose -f ../../docker-compose.yml build
